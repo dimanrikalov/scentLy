8d9051d70edc2619fe81de1074ddd281934082b9
Diman Rikalov <dimanrikalov1@gmail.com>
2022-07-15 23:02:03 +0300

    Update Catalog.js

--- app/client/src/components/Catalog.js
+++ app/client/src/components/Catalog.js
@@ -8,7 +8,7 @@ export default function Catalog () {
             
             <input className="w-1/3 ml-32 mt-10 mb-10 px-8 py-3 rounded-3xl text-lg font-lg bg-gray-100 border border-gray-200 placeholder-gray-600 text-sm focus:outline-none focus:bg-white" type="text" name="catalogSearch" placeholder="Search for a fragrance"/>
             <div className={styles["cards-list"]}>
-                <CatalogCard brand="Parfums De Marly" name="Herod" imgUrl="https://cremedelacreme.shop/cache/images/8f661392d14c63683d746ad55058fb8a/pdm-h-small-bottle-packshot-with-box.jpg"/>
+                <CatalogCard brand="Parfums De Marly" name="Herod" imgUrl="https://hermitage.am/storage/5589/1608036483"/>
                 <CatalogCard brand="Amouage" name="Overture Man" imgUrl="https://cdn.shoplightspeed.com/shops/611681/files/39984173/image.jpg"/>
                 <CatalogCard brand="Creed" name="Royal Oud" imgUrl="https://www.creedfragrances.co.uk/static/media/catalog/product/r/o/royal-oud-100ml_1.jpg"/>
                 <CatalogCard brand="Xerjoff" name="Naxos" imgUrl="https://belodore.rs/media/amasty/amoptmobile/catalog/product/cache/08cd9d1a69d3b81aa4ac293076519bc7/X/E/XE33737-1-Xerjoff_Naxos_100ml_EDP_1_jpg.webp"/>
