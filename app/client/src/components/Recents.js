b236a429bbd2af52a3aa5b140bc79b7c7dc002fd
Diman Rikalov <dimanrikalov1@gmail.com>
2022-07-16 11:01:30 +0300

    Added scrolling to recents div

--- app/client/src/components/Recents.js
+++ app/client/src/components/Recents.js
@@ -5,17 +5,19 @@ import styles from './Recents.module.css'
 export default function Recents() {
     return (
         <div className={styles.wrapper}>
-          <br /><br />
     
-        <h1 style={{margin:'auto', width:'75%'}} className="text-red-500">Recent reviews: </h1>
-        <ul className={styles.cards}>
-
-        <RecentsCard name="Side Effect" brand="Initio" author="Charles Leclerc" description="My absolute favourite from the brand!" imageUrl="../assets/imgs/img-1.jpg" authorImageUrl="../assets/imgs/avatar1.jpg"/>
-        <RecentsCard name="Matsukita" brand="Clive Christian" author="Patrick Bateman" description="Let's see Paul Allen's fragrance!" imageUrl="../assets/imgs/img-2.jpg" authorImageUrl="../assets/imgs/avatar2.jpg"/>
-        <RecentsCard name="Herod" brand="Parfums De Marly" author="James Bond" description="It's Herod. PDM Herod." imageUrl="../assets/imgs/img-3.jpg" authorImageUrl="../assets/imgs/avatar3.jpg"/>
-        <RecentsCard name="Baccarat Rouge 540 Extrait" brand="Maison Francis Kurkdjian" author="Robert De Niro" description="Too boring for me..." imageUrl="../assets/imgs/img-4.jpg" authorImageUrl="../assets/imgs/avatar4.jpg"/>
+        <h1 style={{margin:'auto', width:'75%'}} className="text-red-500 pb-20">Recent reviews: </h1>
         
-      </ul>
+        <div className={styles["recents-div"]}>
+        <ul className={styles.cards}>
+        <li className={styles.card}><RecentsCard name="Side Effect" brand="Initio" author="Charles Leclerc" description="My absolute favourite from the brand!" imageUrl="../assets/imgs/img-1.jpg" authorImageUrl="../assets/imgs/avatar1.jpg"/></li>
+        <li className={styles.card}><RecentsCard name="Matsukita" brand="Clive Christian" author="Patrick Bateman" description="Let's see Paul Allen's fragrance!" imageUrl="../assets/imgs/img-2.jpg" authorImageUrl="../assets/imgs/avatar2.jpg"/></li>
+        <li className={styles.card}><RecentsCard name="Herod" brand="Parfums De Marly" author="James Bond" description="It's Herod. PDM Herod." imageUrl="../assets/imgs/img-3.jpg" authorImageUrl="../assets/imgs/avatar3.jpg"/></li>
+        <li className={styles.card}><RecentsCard name="Baccarat Rouge 540 Extrait" brand="Maison Francis Kurkdjian" author="Robert De Niro" description="Too boring for me..." imageUrl="../assets/imgs/img-4.jpg" authorImageUrl="../assets/imgs/avatar4.jpg"/></li>
+        <li className={styles.card}><RecentsCard name="Baccarat Rouge 540 Extrait" brand="Maison Francis Kurkdjian" author="Robert De Niro" description="Too boring for me..." imageUrl="../assets/imgs/img-4.jpg" authorImageUrl="../assets/imgs/avatar4.jpg"/></li>
+        </ul>
+      </div>
+        
       </div>
     );
 }
