246e0946dbbbb7f43a48df43bf4a6dc0f3813dc6
Diman Rikalov <dimanrikalov1@gmail.com>
2022-07-17 20:27:47 +0300

    Somewhat fixed the resizing of the about page

--- app/client/src/components/About.js
+++ app/client/src/components/About.js
@@ -3,24 +3,25 @@ import styles from './About.module.css';
 export default function About () {
     return (
         
-          <div id="about" className={styles.container}>
+        <div className={styles['min-width']}>
+            <div id="about" className={styles.container}>
           <div className={styles.imageBackground}><img src="./assets/imgs/sitename.png" alt="site-img" className={['pb-20', styles.aboutImage].join(' ')}/></div>
             <div className="row align-items-center mr-auto">
               <div className={["col-md-3", styles.aboutUsText].join(' ')}>
                 <h3 className="section-title text-red-500">About Us</h3>
-                <p className="text-gray-800 pb-2"><strong><u className="text-red-500">ScentLy</u> is the go-to place for keeping up to date with the latest trends in 
+                <p className="text-gray-800"><strong><u className="text-red-500">ScentLy</u> is the go-to place for keeping up to date with the latest trends in 
                     the fragrance industry. The goal is to develop a strong community comprised of people 
                     from <strong><u className="text-red-500">all</u></strong> sides of the spectrum - From newcomers to the most dedicated to the art of 
                     fragrance connoisseurs.</strong></p>
               </div>
-              <div className={["col-sm-6", "col-md-4", "ml-auto", styles.statsCards].join(' ')}>
+              <div className={["col-sm-6", "col-md-4", "ml-auto pt3 pb-4 ", styles.statsCards].join(' ')}>
                         <div className="widget">
                             <div className="icon-wrapper">
                                 <i className="ti-user text-primary" />
                             </div>
                             <div className="infos-wrapper">
                                 <h4 className="text-primary text-4xl">15</h4>
-                                <p className="text-secondary text-gray-900 text-3xl"><strong>Total Users</strong></p>
+                                <p className="text-secondary text-center text-gray-900 text-3xl"><strong>Total Users</strong></p>
                             </div>
                         </div>
                         <div className="widget">
@@ -29,7 +30,7 @@ export default function About () {
                             </div>
                             <div className="infos-wrapper">
                                 <h4 className="text-primary text-4xl">125</h4>
-                                <p className="text-secondary text-gray-900 text-3xl"><strong>Total Reviews</strong></p>
+                                <p className="text-secondary text-center text-gray-900 text-3xl"><strong>Total Reviews</strong></p>
                             </div>
                         </div>
                     </div>
@@ -40,11 +41,12 @@ export default function About () {
                             </div>
                             <div className="infos-wrapper">
                                 <h4 className="text-primary">3434</h4>
-                                <p className="text-secondary text-gray-900"><strong>Total fragrances</strong></p>
+                                <p className="text-secondary text-center text-gray-900"><strong>Total fragrances</strong></p>
                             </div>
                         </div>
                     </div>
                 </div>
           </div>
+        </div>
     )
 }
