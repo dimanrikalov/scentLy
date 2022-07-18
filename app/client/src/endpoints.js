9970d41f4e7bd9e759888ac3f77ca5a46bcdf624
Diman Rikalov <dimanrikalov1@gmail.com>
2022-07-18 17:05:44 +0300

    set up catalog for fetch request

--- app/client/src/endpoints.js
+++ app/client/src/endpoints.js
@@ -2,7 +2,6 @@ const registerUrl = 'http://localhost:3000/auth/register';
 const loginUrl = 'http://localhost:3000/auth/login';
 const catalogUrl = 'http://localhost:3000/catalog';
 
-
 module.exports = { 
     registerUrl, 
     loginUrl, 
