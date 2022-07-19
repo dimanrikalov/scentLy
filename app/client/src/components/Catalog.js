73c8a1e559e3e45bd6fb1588ba418eed8205ba6e
Diman Rikalov <dimanrikalov1@gmail.com>
2022-07-19 12:53:08 +0300

    Added conditional rendering to the catalog page

--- app/client/src/components/Catalog.js
+++ app/client/src/components/Catalog.js
@@ -28,42 +28,17 @@ export default function Catalog() {
                 name="catalogSearch"
                 placeholder="Search for a fragrance"
             />
-            <div className={styles['cards-list']}>
-                {   fragrances ?
-                    fragrances.map(x => <CatalogCard key={x._id} fragrance={x}/>)
-                    : <h1>No fragrances</h1>
+
+            {
+                fragrances.length > 0 ?
+                <div className={styles['cards-list']}>
+                {   
+                    fragrances.map(x => <CatalogCard key={x} fragrance={x}/>)
+                
                 }
-                {/* <CatalogCard
-                    brand="Amouage"
-                    name="Jubilation XXV"
-                    imgUrl="https://images.selfridges.com/is/image/selfridges/476-3001290-JUBLTIONXXVMANEDP_M?$PDP_M_ZOOM$"
-                />
-                <CatalogCard
-                    brand="Roja"
-                    name="Elyisum"
-                    imgUrl="https://cremedelacreme.shop/images/galleries/product_items/1646232306_elysium-creative-kirptas.jpg"
-                />
-                <CatalogCard
-                    brand="Creed"
-                    name="Royal Mayfair"
-                    imgUrl="http://cdn.shopify.com/s/files/1/0271/3298/5441/products/1110081_RM_100ml_1200x1200.jpg?v=1622643258"
-                />
-                <CatalogCard
-                    brand="Parfums De Marly"
-                    name="Layton"
-                    imgUrl="https://images.selfridges.com/is/image/selfridges/496-3003325-PM1600PV_M?$PDP_M_ZOOM$"
-                />
-                <CatalogCard
-                    brand="INITIO"
-                    name="Side Effect"
-                    imgUrl="https://nichegarden.com/wp-content/uploads/2020/05/INITIO-ATOMIC-ROSE-bottel-min.png"
-                />
-                <CatalogCard
-                    brand="Xerjoff"
-                    name="Naxos"
-                    imgUrl="https://belodore.rs/media/amasty/amoptmobile/catalog/product/cache/08cd9d1a69d3b81aa4ac293076519bc7/X/E/XE33737-1-Xerjoff_Naxos_100ml_EDP_1_jpg.webp"
-                /> */}
-            </div>
+                </div>
+                : <h1 className = {mainStyles['no-fragrances']}>No fragrances in database yet.</h1>
+            }
         </div>
     );
 }
