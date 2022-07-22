25adcce0120f60fc929effaee695935a40383246
Diman Rikalov <dimanrikalov1@gmail.com>
2022-07-22 10:06:13 +0300

    created Edit form files

---
+++ app/client/src/components/EditReviewForm.js
@@ -0,0 +1,91 @@
+export default function EditReviewForm () {
+    const { fragranceId } = useParams();
+    const navigate = useNavigate();
+
+    const [fragrance, setFragrance] = useState({});
+
+    const [values, setValues] = useState({
+        description: '',
+        rating: '',
+    });
+
+    const onChangeHandler = (e) => {
+        setValues((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    useEffect(() => {
+        fetch(`${endpoints.catalogUrl}/${fragranceId}/details`)
+            .then((res) => res.json())
+            .then((data) => {
+                setFragrance(data);
+            });
+    }, [fragranceId]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        fetch(`${endpoints.catalogUrl}/${fragranceId}/review/edit`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                fragrance,
+                description: values.description,
+                rating: values.rating,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                navigate(`/fragrance/${fragranceId}/details`);
+            })
+            .catch((err) => console.log(err));
+    };
+    return (
+        <div className={styles['create-form']}>
+            <div className={styles['left-side']}>
+                <img src={fragrance.imageUrl} alt="" />
+            </div>
+
+            <div className={styles['right-side']}>
+                <form>
+                    <h1 className={styles['text']}>Create a review</h1>
+                    <div className="mx-auto max-w-xs">
+                        <h4 className={styles['subtitle']}>
+                            Review description:{' '}
+                        </h4>
+                        <textarea
+                            name="description"
+                            cols="30"
+                            rows="10"
+                            className={styles.description}
+                            value={values.description}
+                            onChange={onChangeHandler}
+                            placeholder="Enter a review"
+                        ></textarea>
+                        <h4 className={styles['subtitle']}>Review score: </h4>
+                        <input
+                            type="number"
+                            name="rating"
+                            className={styles.rating}
+                            value={values.rating}
+                            onChange={onChangeHandler}
+                            placeholder="    [1-5]"
+                        />
+                        <button
+                            type="submit"
+                            className={styles['submit-button']}
+                            onClick={submitHandler}
+                        >
+                            <span className="ml-3 text-lg">Create</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
