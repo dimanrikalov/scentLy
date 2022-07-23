4a6efd3ce41fd59199a471c6807225950dd3e946
Diman Rikalov <dimanrikalov1@gmail.com>
2022-07-23 15:43:57 +0300

    Added profile dynamic information

--- app/client/src/components/Profile.js
+++ app/client/src/components/Profile.js
@@ -1,35 +1,40 @@
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
 import Owned from './Owned';
 import styles from './Profile.module.css';
 
 export default function Profile() {
+
+    const {user, setUser} = useContext(UserContext);
+    
+
     return (
         <div id="profile" className={styles['profilePage']}>
             <div className={styles['profileDiv']}>
-                <h1>Diman Rikalov</h1>
+                <h1>{user.name}</h1>
 
                 <img
                     className={styles.profileImg}
-                    src="../assets/imgs/avatar1.jpg"
-                    alt="profile-pic"
+                    src={user.profileImage}
                 />
 
                 <div>
                     <h2 className="pb-3">Profile Information:</h2>
                     <ul>
                         <li>
-                            <h5>Age: 21</h5>
+                            <h5>Age: {user.age}</h5>
                         </li>
                         <li>
-                            <h5>Gender: Male</h5>
+                            <h5>Gender: {user.gender}</h5>
                         </li>
                         <li>
-                            <h5>Location: Plovdiv, Bulgaria</h5>
+                            <h5>Location: {user.city}, {user.country}</h5>
                         </li>
                         <li>
-                            <h5>Fragrances owned: 15</h5>
+                            <h5>Fragrances owned: {user.ownedFragrances.length}</h5>
                         </li>
                         <li>
-                            <h5>Review count: 47</h5>
+                            <h5>Review count: {user.reviews.length}</h5>
                         </li>
                     </ul>
                 </div>
