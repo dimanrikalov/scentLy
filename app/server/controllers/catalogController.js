19f665071baaecf1d3ad59942887f1d5414256bd
Diman Rikalov <dimanrikalov1@gmail.com>
2022-07-25 17:28:07 +0300

    Added try catch to catalog controller

--- app/server/controllers/catalogController.js
+++ app/server/controllers/catalogController.js
@@ -4,19 +4,31 @@ const reviewService = require('../services/fragranceReviewService');
 const userService = require('../services/userService');
 
 router.get('/', async (req, res) => {
-    const data = await api.getAll();
-    res.json(data);
+    try {
+        const data = await api.getAll();
+        res.json(data);
+    } catch (err) {
+        return res.status(400).json({message: 'Server error! Try again later!'})
+    }
 });
 
 router.get('/reviews', async (req, res) => {
-    const data = await reviewService.getAllDetailed();
-    res.json(data);
+    try {
+        const data = await reviewService.getAllDetailed();
+        res.json(data);
+    } catch (err) {
+        res.status(400).json({message: 'Server error! Try again later!'});
+    }
 });
 
 router.post('/search', async (req, res) => {
-    const searchString = req.body.catalogSearch;
-    const validFragrances = await api.getAllThatHave(searchString);
-    return res.json(validFragrances);
+    try {
+        const searchString = req.body.catalogSearch;
+        const validFragrances = await api.getAllThatHave(searchString);
+        return res.json(validFragrances);
+    } catch (err) {
+        return res.status(400).json({message: 'Server error! Try again later!'});
+    }
 });
 
 router.post('/create', async (req, res) => {
@@ -51,43 +63,47 @@ router.get('/:fragranceId/details', async (req, res) => {
     });
 });
 
-router.get('/:fragranceId/edit', async (req, res) => {
-    res.json({ [req.params.fragranceId]: 'edit' });
-});
-
 router.post('/:fragranceId/edit', async (req, res) => {
-    const fragrance = await api.getById(req.params.fragranceId);
+    try {
+        const fragrance = await api.getById(req.params.fragranceId);
 
-    if (!fragrance) {
-        return res.status(404).json({
-            message: `Fragrance with id: ${req.params.fragranceId} not found!`,
-        });
+        if (!fragrance) {
+            return res.status(404).json({
+                message: `Fragrance with id: ${req.params.fragranceId} not found!`,
+            });
+        }
+        await api.updateById(req.params.fragranceId, { ...req.body });
+        res.json(fragrance);
+    } catch (err) {
+        return res.status(400).json({message: 'Server error: Could complete editing operation'});
     }
-    await api.updateById(req.params.fragranceId, { ...req.body });
-
-    res.json({ [req.params.fragranceId]: 'edited' });
 });
 
 router.get('/:fragranceId/delete', async (req, res) => {
-    const fragranceToDelete = await api.getById(req.params.fragranceId);
-    const creator = await userService.getById(fragranceToDelete.author);
-
-    await Promise.all(
-        fragranceToDelete.reviews.map(async (x) => {
-            await reviewService.deleteReview(x._id);
-        })
-    );
-
-    creator.ownedFragrances = creator.ownedFragrances.filter(
-        (x) => x != req.params.fragranceId
-    );
-    await userService.updateById(creator._id, creator);
-
-    await api.deleteById(req.params.fragranceId);
-    res.json({ [req.params.fragranceId]: 'deleted', creator });
+    try {
+        const fragranceToDelete = await api.getById(req.params.fragranceId);
+        const creator = await userService.getById(fragranceToDelete.author);
+    
+        await Promise.all(
+            fragranceToDelete.reviews.map(async (x) => {
+                await reviewService.deleteReview(x._id);
+            })
+        );
+    
+        creator.ownedFragrances = creator.ownedFragrances.filter(
+            (x) => x != req.params.fragranceId
+        );
+        await userService.updateById(creator._id, creator);
+    
+        await api.deleteById(req.params.fragranceId);
+        res.json({ [req.params.fragranceId]: 'deleted', creator });
+    } catch (err) {
+        return res.status(404).json({message: 'Server error: Could not complete deleting operation'})
+    }
 });
 
 router.post('/:fragranceId/review/create', async (req, res) => {
+    
     const fragrance = await api.getById(req.params.fragranceId);
     const user = await userService.getById(req.body.author);
 
@@ -101,21 +117,25 @@ router.post('/:fragranceId/review/create', async (req, res) => {
         return res.status(400).json({ message: 'Invalid rating!' });
     }
 
-    const newReview = await reviewService.createReview({
-        ...req.body,
-        imageUrl: fragrance.imageUrl,
-        fragranceName: fragrance.name,
-        fragranceBrand: fragrance.brand,
-    });
-    fragrance.reviews.push(newReview._id);
-    const total =
-        (fragrance.rating + newReview.rating) / fragrance.reviews.length;
-    fragrance.rating = total;
-    await api.updateById(req.params.fragranceId, fragrance);
-
-    user.reviews.push(newReview._id);
-    await userService.updateById(user._id, user);
-    res.json({ [req.params.fragranceId]: 'reviewed', user });
+    try {
+        const newReview = await reviewService.createReview({
+            ...req.body,
+            imageUrl: fragrance.imageUrl,
+            fragranceName: fragrance.name,
+            fragranceBrand: fragrance.brand,
+        });
+        fragrance.reviews.push(newReview._id);
+        const total =
+            (fragrance.rating + newReview.rating) / fragrance.reviews.length;
+        fragrance.rating = total;
+        await api.updateById(req.params.fragranceId, fragrance);
+    
+        user.reviews.push(newReview._id);
+        await userService.updateById(user._id, user);
+        res.json({ [req.params.fragranceId]: 'reviewed', user });
+    } catch (err) {
+        return res.status(404).json({message: 'Server error: Could not create review!'})
+    }
 });
 
 router.post('/:fragranceId/review/edit', async (req, res) => {
@@ -152,13 +172,19 @@ router.post('/:fragranceId/review/edit', async (req, res) => {
         isFound.description = req.body.description;
         isFound.rating = req.body.rating;
 
-        await api.updateById(fragrance._id, fragrance);
-        await reviewService.updateReview(isFound._id, {
-            description: req.body.description,
-            rating: req.body.rating
-        });
+        try{
+            await api.updateById(fragrance._id, fragrance);
+            await reviewService.updateReview(isFound._id, {
+                description: req.body.description,
+                rating: req.body.rating
+            });
 
-        res.json(fragrance);
+            res.json(fragrance);
+        } catch(err) {
+            res.status(400).json({message: 'Could not modify the data!'});
+        }
+    } else {
+        return res.status(404).json({message: 'Review not found!'});
     }
 });
 
@@ -187,15 +213,19 @@ router.post('/:fragranceId/review/delete', async (req, res) => {
             fragrance.reviews.forEach((x) => (sum += x.rating));
             fragrance.rating = sum / fragrance.reviews.length;
         }
-        await api.updateById(req.params.fragranceId, fragrance);
+        try {
+            await api.updateById(req.params.fragranceId, fragrance);
 
-        user.reviews = user.reviews.filter(
-            (x) => x.fragrance.toString() != fragrance._id.toString()
-        );
-        await userService.updateById(user._id, user);
+            user.reviews = user.reviews.filter(
+                (x) => x.fragrance.toString() != fragrance._id.toString()
+            );
+            await userService.updateById(user._id, user);
 
-        await reviewService.deleteReview(isFound._id);
-        res.json({ [req.params.fragranceId]: 'deleted', user, fragrance });
+            await reviewService.deleteReview(isFound._id);
+            res.json({ [req.params.fragranceId]: 'deleted', user, fragrance });
+        } catch (err) {
+            res.json(400).json({message: 'Could not update the information!'});
+        }
     } else {
         return res
             .status(404)
