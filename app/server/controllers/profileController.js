844dff5d6b9e2208bf0c154f5259d6ea715c120a
Diman Rikalov <dimanrikalov1@gmail.com>
2022-07-25 17:28:23 +0300

    added try catch to profile controller

--- app/server/controllers/profileController.js
+++ app/server/controllers/profileController.js
@@ -2,9 +2,14 @@ const router = require('express').Router();
 const userService = require('../services/userService');
 
 router.get('/:userId', async (req, res) => {
-    const user = await userService.getByIdDetailed(req.params.userId);
-    res.json(user);
+    try {
+        const user = await userService.getByIdDetailed(req.params.userId);
+        res.json(user);
+    } catch (err) {
+        return res
+            .status(404)
+            .json({ message: 'Server error.Could not get user information!' });
+    }
 });
-
 
 module.exports = router;
