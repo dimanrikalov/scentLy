dccc8af2dbcac2b423b2501c6fe72a0e35fe5119
Diman Rikalov <dimanrikalov1@gmail.com>
2022-08-01 13:20:33 +0300

    Added proper spacing

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -55,23 +55,23 @@ function App() {
             <UserContext.Provider value={userValue}>
             <Navbar />
             <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='*' element={<ErrorPage/>}/>
-                <Route path='/about' element={<About/>}/>
-                <Route path='/catalog' element={<Catalog/>}/>
-                <Route path='/fragrance/:fragranceId/details' element={<FragranceDetails/>}/>
+                <Route path='/' element={<Home/>} />
+                <Route path='*' element={<ErrorPage/>} />
+                <Route path='/about' element={<About/>} />
+                <Route path='/catalog' element={<Catalog />} />
+                <Route path='/fragrance/:fragranceId/details' element={<FragranceDetails/>} />
 
                 <Route element={<GuestGuard />}>
-                    <Route path='/auth/login' element={<Login/>}/>
-                    <Route path='/auth/register' element={<Register/>}/>
+                    <Route path='/auth/login' element={<Login/>} />
+                    <Route path='/auth/register' element={<Register />} />
                 </Route>
 
                 <Route element={<UserGuard />}>
-                    <Route path='/auth/profile' element={<Profile/>}/>
-                    <Route path='/fragrance/create' element={<CreateFragranceForm/>}/>
-                    <Route path='/fragrance/:fragranceId/edit' element={<EditFragranceForm/>}/>
-                    <Route path='/fragrance/:fragranceId/review/edit' element={<EditReviewForm/>}/>
-                    <Route path='/fragrance/:fragranceId/review/create' element={<CreateReviewForm/>}/>
+                    <Route path='/auth/profile' element={<Profile />} />
+                    <Route path='/fragrance/create' element={<CreateFragranceForm />} />
+                    <Route path='/fragrance/:fragranceId/edit' element={<EditFragranceForm />} />
+                    <Route path='/fragrance/:fragranceId/review/edit' element={<EditReviewForm />} />
+                    <Route path='/fragrance/:fragranceId/review/create' element={<CreateReviewForm />} />
                 </Route>
 
             </Routes>
