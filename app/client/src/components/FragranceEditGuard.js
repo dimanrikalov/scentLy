22e4e7a07cd53b3916c3dae5054199bd50ef83da
Diman Rikalov <dimanrikalov1@gmail.com>
2022-08-05 16:18:05 +0300

    Added fragrance edit guard

---
+++ app/client/src/components/FragranceEditGuard.js
@@ -0,0 +1,38 @@
+import Loading from "./Loading";
+import endpoints from '../endpoints';
+import { Outlet,Navigate } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
+import { useContext, useEffect, useState } from "react";
+
+export default function FragranceEditGuard() {
+    const {user} = useContext(UserContext); 
+    const [isLoading, setIsLoading] = useState(true);
+    const [isFragranceCreator, setIsFragranceCreator] = useState(false);
+    let fragranceId = window.location.pathname.slice(11);
+    fragranceId = fragranceId.slice(0, fragranceId.indexOf('/'));
+
+    useEffect(() => {
+        fetch(`${endpoints.catalogUrl}/${fragranceId}/details`)
+                    .then(res => res.json())
+                    .then(data => {
+                        setIsLoading(false);
+                        if(data.author.toString() == user._id.toString()) {
+                            setIsFragranceCreator(true);
+                        }
+                    })
+                    .catch(() => {
+                        setIsLoading(false);
+                        setIsFragranceCreator(false);
+                    });
+    }, [])
+
+    if(isLoading) {
+        return <Loading />
+    } else {
+        if(isFragranceCreator) {
+            return <Outlet />
+        }
+        
+        return <Navigate to={`/fragrance/${fragranceId}/details`} replace />
+    }
+}
