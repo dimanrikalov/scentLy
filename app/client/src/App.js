04289477f1292668d8f89f5508997cd93d264c8f
Diman Rikalov <dimanrikalov1@gmail.com>
2022-08-05 13:12:38 +0300

    moved user authentication logic out of app component into userContext component

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,4 +1,3 @@
-import endpoints from './endpoints';
 import Home from './components/Home';
 import styles from './App.module.css';
 import About from './components/About';
@@ -12,8 +11,6 @@ import ErrorPage from './components/ErrorPage';
 import UserGuard from './components/UserGuard';
 import GuestGuard from './components/GuestGuard';
 import { Routes, Route } from 'react-router-dom';
-import {UserContext} from './contexts/UserContext';
-import { useEffect, useMemo, useState } from 'react';
 import EditReviewForm from './components/EditReviewForm';
 import CreateReviewForm from './components/CreateReviewForm';
 import FragranceDetails from './components/FragranceDetails';
@@ -22,37 +19,9 @@ import CreateFragranceForm from './components/CreateFragranceForm';
 
 function App() {
 
-    const [user, setUser] = useState(null);
-    const userValue = useMemo(() => ({user, setUser}), [user, setUser]);
-
-    useEffect(()=>{
-        const isLogged = JSON.parse(localStorage.getItem('user'));
-
-        if(isLogged) {
-            fetch(`${endpoints.getUserUrl}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ 
-                    userId : isLogged._id
-                })
-            })
-            .then(res => res.json())
-            .then(data => {
-                setUser(data);
-            })
-            .catch(() => {
-                localStorage.clear();
-                setUser(null);
-            });
-        }
-    },[])
-
     return (
         <div className={styles['main-backround']}>
 
-            <UserContext.Provider value={userValue}>
             <Navbar />
             <Routes>
                 <Route path='/' element={<Home />} />
@@ -61,23 +30,20 @@ function App() {
                 <Route path='/catalog' element={<Catalog />} />
                 <Route path='/fragrance/:fragranceId/details' element={<FragranceDetails />} />
 
-                {/* <Route element={<GuestGuard />}> */}
-                    <Route path='/auth/login' element={<Login />} />
-                    <Route path='/auth/register' element={<Register />} />
-                {/* </Route> */}
-              
+                <Route path='/auth/login' element={<Login />} />
+                <Route path='/auth/register' element={<Register />} />
 
-                {/* <Route element={<UserGuard />}> */}
+                <Route element={<GuestGuard />}>
+               
                     <Route path='/auth/profile' element={<Profile />} />
                     <Route path='/fragrance/create' element={<CreateFragranceForm />} />
                     <Route path='/fragrance/:fragranceId/edit' element={<EditFragranceForm />} />
                     <Route path='/fragrance/:fragranceId/review/edit' element={<EditReviewForm />} />
                     <Route path='/fragrance/:fragranceId/review/create' element={<CreateReviewForm />} />
-                {/* </Route> */}
+                </Route>
 
             </Routes>
             <Footer />
-            </UserContext.Provider>
 
         </div>
     );
--- app/client/src/contexts/UserContext.js
+++ app/client/src/contexts/UserContext.js
@@ -1,3 +1,43 @@
-import { createContext } from "react";
-
+import { Children, createContext } from "react";
+import { useEffect, useState } from "react";
+import endpoints from '../endpoints';
 export const UserContext = createContext(null);
+
+
+
+const UserProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    // const userValue = useMemo(() => ({user, setUser}), [user, setUser]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(()=>{
+        const isLogged = JSON.parse(localStorage.getItem('user'));
+
+        if(isLogged) {
+            fetch(`${endpoints.getUserUrl}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ 
+                    userId : isLogged._id
+                })
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setUser(data);
+                setLoading(false);
+            })
+            .catch(() => {
+                localStorage.clear();
+                setUser(null);
+                setLoading(false);
+            });
+        }
+    },[])
+
+    return <UserContext.Provider value={{user, setUser, loading}}>{children}</UserContext.Provider>
+}
+
+export default UserProvider;
