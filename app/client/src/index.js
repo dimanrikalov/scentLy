817baef65e246394f9eeb6bd9fc458689e776c9f
Diman Rikalov <dimanrikalov1@gmail.com>
2022-08-05 13:12:13 +0300

    Removed React Strict and added userContext

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -2,12 +2,14 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import UserProvider from './contexts/UserContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <UserProvider>
+                <App />
+            </UserProvider>
         </BrowserRouter>
-    </React.StrictMode>
 );
